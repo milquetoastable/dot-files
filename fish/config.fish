a696af9001c4acd2bbca5115bcff5fa7ed46f21d
Mike <5898731+milquetoastable@users.noreply.github.com>
2022-07-08 18:11:38 +0100

    Initial commit

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+	set -gx HOMEBREW_PREFIX "/opt/homebrew";
+	set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+	set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
+	set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
+	set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
+	set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
+
+	starship init fish | source
+	source /opt/homebrew/opt/asdf/libexec/asdf.fish
+	fzf_configure_bindings --directory=\cf --history=\cs --git_log=\cl --git_status=\cg --processes=\cp
+end
